7ea5d1426ba30fbaedce25f61807c37e9d7a1bc8
griffinross2 <griffinross2@gmail.com>
2025-03-21 17:57:38 -0600

    Start setting up to compile and run C on the CPU

---
+++ Code/main.c
@@ -0,0 +1,14 @@
+void main(void) {
+    int a = 2;
+    int b = 3;
+    int c = a + b;
+    *((int *)0x4000) = c;
+
+    // Halt
+    asm("ebreak");
+}
+
+void Exception_Handler() {
+    while(1) {
+    }
+}
