cd34850928be7f8ceb20c7b9f98c062f27816243
griffinross2 <griffinross2@gmail.com>
2025-03-21 19:15:50 -0600

    C code for vectored interrupts

--- Code/compile.bat
+++ Code/compile.bat
@@ -3,3 +3,4 @@ riscv-none-elf-as -march=rv32im_zicsr -mabi=ilp32 -o startup.o startup.S
 riscv-none-elf-ld -T linkerscript.ld --check-sections -o main.elf main.o startup.o 
 riscv-none-elf-objdump -D main.elf > main.S
 riscv-none-elf-objcopy -O ihex main.elf main.hex
+python hextomemfile.py
